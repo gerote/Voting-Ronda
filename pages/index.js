7e5fea77cd21e20d321b283a0528ac7b5665e441
gerote <85430230+gerote@users.noreply.github.com>
2025-12-13 18:45:40 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,42 @@ export default function Home() {
     <div
       style={{
         width: "100vw",
-        minHeight: "100vh",
+        height: "100vh",
         margin: 0,
         padding: 0,
-        background: "#f5f5f5",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        background: "#000",
+        overflow: "hidden",
+        position: "relative",
       }}
     >
-
-      {/* VIDEO */}
-      <div
+      {/* FULLSCREEN BACKGROUND VIDEO */}
+      <iframe
+        src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i&autoplay=0&controls=1&modestbranding=1&rel=0&playsinline=1"
         style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
           width: "100%",
-          height: "40vh",
-          background: "#000",
-          overflow: "hidden",
+          height: "100%",
+          border: "none",
+          objectFit: "cover",
         }}
-      >
-        <iframe
-          src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i&autoplay=0&controls=1&modestbranding=1&rel=0"
-          style={{
-            width: "100%",
-            height: "100%",
-            border: "none",
-          }}
-          allow="encrypted-media"
-          allowFullScreen
-        ></iframe>
-      </div>
+        allow="encrypted-media"
+        allowFullScreen
+      />
 
-      {/* GAMBAR */}
+      {/* FLOATING CARD */}
       <div
         style={{
-          width: "100%",
-          maxWidth: "600px",
-          marginTop: "20px",
-          padding: "10px",
+          position: "absolute",
+          bottom: "5%",
+          right: "5%",
+          width: "340px",
+          background: "rgba(255,255,255,0.95)",
+          backdropFilter: "blur(8px)",
+          borderRadius: "12px",
+          padding: "20px",
+          boxShadow: "0 6px 24px rgba(0,0,0,0.25)",
         }}
       >
         <img
@@ -49,11 +47,9 @@ export default function Home() {
           style={{
             width: "100%",
             borderRadius: "8px",
-            border: "1px solid #ddd",
           }}
         />
       </div>
-
     </div>
   );
 }
