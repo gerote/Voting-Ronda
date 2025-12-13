35755652e9d91d10422930d5ada09294a5502ba8
gerote <85430230+gerote@users.noreply.github.com>
2025-12-13 18:42:56 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,57 @@ export default function Home() {
     <div
       style={{
         width: "100vw",
-        height: "100vh",
+        minHeight: "100vh",
         margin: 0,
         padding: 0,
-        background: "#000",
-        overflow: "hidden",
+        background: "#f5f5f5",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <iframe
-        src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i?autoplay=0&controls=1&modestbranding=1&showinfo=0&rel=0&playsinline=1"
+
+      {/* VIDEO */}
+      <div
         style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          border: "none",
+          width: "100%",
+          height: "40vh",
+          background: "#000",
+          overflow: "hidden",
         }}
-        allow="encrypted-media"
-        allowFullScreen
-      ></iframe>
+      >
+        <iframe
+          src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i&autoplay=0&controls=1&modestbranding=1&rel=0"
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "none",
+          }}
+          allow="encrypted-media"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      {/* GAMBAR */}
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "600px",
+          marginTop: "20px",
+          padding: "10px",
+        }}
+      >
+        <img
+          src="https://i.imgur.com/lbOYAlc.png"
+          alt="Hasil Voting"
+          style={{
+            width: "100%",
+            borderRadius: "8px",
+            border: "1px solid #ddd",
+          }}
+        />
+      </div>
+
     </div>
   );
 }
