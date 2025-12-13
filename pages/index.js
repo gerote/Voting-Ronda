b203f893182184379e5ba0b3bc68475b698f34b7
gerote <85430230+gerote@users.noreply.github.com>
2025-12-13 18:32:07 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,28 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Video</title>
-  <style>
-    html, body {
-      margin: 0;
-      padding: 0;
-      width: 100%;
-      height: 100%;
-      background: #000;
-      overflow: hidden;
-    }
-    iframe {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      border: none;
-    }
-  </style>
-</head>
-<body>
-  <iframe
-    src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i?autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&rel=0&playsinline=1"
-    allow="autoplay; encrypted-media"
-    allowfullscreen
-  ></iframe>
-</body>
-</html>
+export default function Home() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: "#000",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i?autoplay=1&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0&playsinline=1"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          border: "none",
+        }}
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
