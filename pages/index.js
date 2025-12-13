6f3156c5a3e7a19bcd5fd1eea24646a812328441
gerote <85430230+gerote@users.noreply.github.com>
2025-12-13 18:35:47 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ export default function Home() {
       style={{
         width: "100vw",
         height: "100vh",
-        background: "#000",
         margin: 0,
         padding: 0,
+        background: "#000",
         overflow: "hidden",
       }}
     >
       <iframe
-        src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i?autoplay=1&mute=1&controls=0&modestbranding=1&showinfo=0&rel=0&playsinline=1"
+        src="https://www.youtube.com/embed/CKDxmMIhxI4?si=B54XGNYGF-KD6J7i?autoplay=0&controls=1&modestbranding=1&showinfo=0&rel=0&playsinline=1"
         style={{
           position: "fixed",
           top: 0,
@@ -20,7 +20,7 @@ export default function Home() {
           height: "100vh",
           border: "none",
         }}
-        allow="autoplay; encrypted-media"
+        allow="encrypted-media"
         allowFullScreen
       ></iframe>
     </div>
